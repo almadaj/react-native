5a9aef92934a6ff2af5e9532a2bed9684c78faca
Juliano Almada <almadaj@edu.unifor.br>
2024-01-16 16:41:14 -0300

    hello world em reactnative

---
+++ base-rn/App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Olá Mundo!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 40,
+    color: "#f64348",
+  },
+});
